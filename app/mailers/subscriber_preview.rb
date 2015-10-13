ec95ab6b43a507c958f61753825a277140d63f26
Galih Muhammad <galih0muhammad@gmail.com>
2015-10-13 23:29:37 +0700

    attempt to debug error of Mailview loaded in production

--- app/mailers/subscriber_preview.rb
+++ app/mailers/subscriber_preview.rb
@@ -1,11 +1,13 @@
-class SubscriberPreview < MailView
+if Rails.env.development?
+	class SubscriberPreview < MailView
 
-  # Pull data from existing fixtures / dev data
-  def welcome
-    email = "galih0muhammad@gmail.com"
-    mail = SubscriberMailer.welcome(email)
-    mail
-  end
+	  # Pull data from existing fixtures / dev data
+	  def welcome
+	    email = "galih0muhammad@gmail.com"
+	    mail = SubscriberMailer.welcome(email)
+	    mail
+	  end
 
 
+	end
 end
