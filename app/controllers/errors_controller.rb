f5e1fb72f6c77149829146fb55b03d8c1f1d6089
Galih Muhammad <galih0muhammad@gmail.com>
2016-04-12 16:01:53 +0700

    remove normal_nav from error controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,16 +1,18 @@
 class ErrorsController < ApplicationController
   skip_before_action :authenticate_user!
-  before_action :normal_nav
   
   def not_found
+    @static = true
   	render(:status => 404)
   end
 
   def unprocessable
+    @static = true
   	render(:status => 422)
   end
 
   def internal_server_error
+    @static = true
   	render(:status => 500)
   end
 
